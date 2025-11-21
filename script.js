e77722edb8cd13501ab7caff83068ca59f3cda1a
vohoangtanphat1001-dev <vohoangtanphat1001@gmail.com>
2025-11-21 14:08:58 +0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio của Võ Hoàng Tấn Phát đã load xong!');
-    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
-});
