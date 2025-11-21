a1335970e7fa0b5fea63430f1f51345f670015dc
vohoangtanphat1001-dev <vohoangtanphat1001@gmail.com>
2025-11-21 13:43:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Võ Hoàng Tấn Phát đã load xong!');
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+});
